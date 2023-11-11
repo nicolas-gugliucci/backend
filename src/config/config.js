44c4ed036ec5f4cc8353be603f0a6de82d22a3bc
Nicolas Gugliucci <gugliucci.nicolas@gmail.com>
2023-11-11 13:51:53 -0300

    ajustes

--- src/config/config.js
+++ src/config/config.js
@@ -2,11 +2,32 @@ import dotenv from 'dotenv'
 import params from './params.js'
 import nodemailer from 'nodemailer'
 
-const environment = 'production'
 
-dotenv.config({
-    path: './.env'//'DEVELOPMENT'?'./.env.development':'./.env.production'
-})
+const environment = params.mode;
+
+// Railway proporciona automáticamente las variables de entorno
+dotenv.config();
+
+// Ahora, puedes acceder a las variables de entorno directamente
+// sin depender de archivos .env específicos para cada entorno
+
+// Ejemplo de cómo puedes usar la variable de entorno NODE_ENV
+let PORT_ENV
+let LOGGER_LEVEL
+if (environment.toUpperCase() === 'DEVELOPMENT') {
+    PORT_ENV = process.env.PORT_DEV
+    LOGGER_LEVEL = process.env.LOGGER_LEVEL_DEV
+} else {
+    PORT_ENV = process.env.PORT_PROD
+    LOGGER_LEVEL = process.env.LOGGER_LEVEL_PROD
+}
+
+
+// const environment = 'production'
+
+// dotenv.config({
+//     path: './.env'//'DEVELOPMENT'?'./.env.development':'./.env.production'
+// })
 
 // const environment = (params.mode)
 
@@ -18,8 +39,6 @@ const DATABASE_URL = process.env.DATABASE_URL
 const MONGO_STORE_SECRET = process.env.MONGO_STORE_SECRET
 const CLIENT_ID = process.env.CLIENT_ID
 const CLIENT_SECRET = process.env.CLIENT_SECRET
-const PORT_ENV = process.env.PORT
-const LOGGER_LEVEL = process.env.LOGGER_LEVEL
 const MAIL_PASSWORD = process.env.MAIL_PASSWORD
 const MAIL = process.env.MAIL
 const HOST = process.env.HOST
