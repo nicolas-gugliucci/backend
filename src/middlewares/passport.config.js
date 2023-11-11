22465307109e1a03cecbf0d8d37050bf31a8a618
Nicolas Gugliucci <gugliucci.nicolas@gmail.com>
2023-11-11 18:00:30 -0300

    ajustes

--- src/middlewares/passport.config.js
+++ src/middlewares/passport.config.js
@@ -36,6 +36,7 @@ export const initPassport = () => {
                     const protocolo = req.protocol;
                     const host = req.get('host');
                     const currentUrl = `${protocolo}://${host}`;
+                    console.log(currentUrl)
                     fetch(`${currentUrl}/api/carts`,{
                         method:'POST',
                     }).then(result => {
@@ -43,8 +44,6 @@ export const initPassport = () => {
                             return result.json();
                         }
                     }).then(async data => {
-                        console.log(data)
-                        console.log(data.payload)
                         let cid = data.payload._id
                         const user= {
                             first_name,
